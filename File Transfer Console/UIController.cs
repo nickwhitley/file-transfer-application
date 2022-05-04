c000c9e7ea12e44478069ec0b676549eace803f9
nickwhitley <nicrofilm@gmail.com>
2022-05-04 14:27:26 -0400

    added controller and data class.

---
+++ File Transfer Console/UIController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Transfer_Console
+{
+    internal class UIController
+    {
+    }
+}
