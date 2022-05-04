2e5956866f3f186e31427996687a93502a690835
nickwhitley <nicrofilm@gmail.com>
2022-05-04 06:07:47 -0400

    initial console application

---
+++ File Transfer Console/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter origin folder path:");
+string originFolderPath = Console.ReadLine();
+Console.WriteLine("Please enter transfer folder path:");
+string transferFolderPath = Console.ReadLine();
+Console.WriteLine($"\nOrigin Folder Path: {originFolderPath}" +
+    $"\nTransfer Folder Path: {transferFolderPath}" +
+    $"\npress ENTER to begin transfer.");
+if(Console.ReadKey().Key == ConsoleKey.Enter)
+{
+    Console.WriteLine("Beginning Transfer...");
+}
+
