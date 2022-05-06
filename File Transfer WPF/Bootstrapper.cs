660d4a307474ef0b390dc13b730dd03656f0e3a6
nickwhitley <nicrofilm@gmail.com>
2022-05-06 05:42:36 -0400

    Layed out MVVM and setup basic Caliburn.Micro configuration.

---
+++ File Transfer WPF/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using File_Transfer_WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace File_Transfer_WPF
+{
+    internal class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
